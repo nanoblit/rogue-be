e68cb0e8e22032b4d3cdda583d2990ff69ddfa81
nanoblit <nanoblit@users.noreply.github.com>
2019-11-02 11:47:15 +0100

    Setup socket.io

--- build/index.js
+++ build/index.js
@@ -1,3 +1,23 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var http_1 = __importDefault(require("http"));
+var socket_io_1 = __importDefault(require("socket.io"));
+var app = express_1.default();
+var http = http_1.default.createServer(app);
+var io = socket_io_1.default(http);
 console.log("test");
+io.on("connection", function (socket) {
+    console.log("a user connected");
+    socket.emit("test", "hello world");
+    socket.on("disconnect", function () {
+        console.log("user disconnected");
+    });
+});
+http.listen(8081, function () {
+    console.log("Initialize completed, server running on port 8081");
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("test");
+import _app from "express";
+import _http from "http";
+import _io from "socket.io";
+
+const app = _app();
+const http = _http.createServer(app);
+const io = _io(http);
+
+io.on("connection", socket => {
+  console.log("a user connected");
+  socket.emit("test", "hello world");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+http.listen(8081, () => {
+  console.log("Initialize completed, server running on port 8081");
+});
